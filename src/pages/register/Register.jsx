1ecc91ff7d50a212a2437d5b87faf7d48cb0315a
Burak Karaman <karaman.buraak@gmail.com>
2024-03-23 17:32:29 +0100

    Update Register Page

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
-// import registerLogo from '../../assets/under-construction.png'
+import registerLogo from '../../assets/under-construction.png'
 import { ImageStyle } from './Style'
-import HomeStyle from '../home/Style'
 
 const Register = () => {
   return (
-    <HomeStyle>
-      {/* <img src={registerLogo} alt="" /> */}
-    </HomeStyle>
+      <ImageStyle>
+      <img src={registerLogo} alt="" style={{width:"100%", height:"930px"}}/>
+      </ImageStyle>
   )
 }
 
