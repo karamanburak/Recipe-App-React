d93aeb393d0ae4997ab39aa624eaee2536e8201e
Burak Karaman <karaman.buraak@gmail.com>
2024-03-23 17:10:35 +0100

    Create Pages Structure

---
+++ src/pages/register/Register.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+// import registerLogo from '../../assets/under-construction.png'
+import { ImageStyle } from './Style'
+import HomeStyle from '../home/Style'
+
+const Register = () => {
+  return (
+    <HomeStyle>
+      {/* <img src={registerLogo} alt="" /> */}
+    </HomeStyle>
+  )
+}
+
+export default Register
---
+++ src/pages/register/Style.jsx
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+
+export const HomeStyle = styled.div`
+width: 100%;
+height: 100vh;
+padding: .5rem;
+gap:.5rem;
+border: 2px solid #fff;
+background: #F1F2B5; 
+background: -webkit-linear-gradient(to right, #135058, #F1F2B5);  
+background: linear-gradient(to right, #135058, #F1F2B5); 
+`;
+
+
+export const ImageStyle = styled.image`
+display: block;
+text-align: center;
+margin-top: 2rem;
+`
